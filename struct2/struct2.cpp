95d63d2aae567074a33affb4dbfce1e842652b8a
hi-lufz1 <latifufauzi@gmail.com>
2023-04-03 14:48:36 +0700

    6

---
+++ struct2/struct2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct AlamatDetail {
+	string desa;
+	string kota;
+ };
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
