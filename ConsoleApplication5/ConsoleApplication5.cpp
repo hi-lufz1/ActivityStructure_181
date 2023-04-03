447758717e8f0d268b7fc99ea182c240abe54cd4
hi-lufz1 <latifufauzi@gmail.com>
2023-04-03 14:38:02 +0700

    5

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -17,4 +17,29 @@ int main()
 	mhs1.alamat = "yogyakarta";
 	mhs1.umur = 19;
 
+
+	cout << "\nMasukkan nim :";
+		cin >> mhs2.nim;
+		cout << "\nMasukkan nama :";
+		cin >> mhs2.nama;
+		cout << "\nMasukkan alamat :";
+		cin >> mhs2.alamat;
+		cout << "\nMasukkan umur :";
+		cin >> mhs2.umur;
+		
+
+		cout << "\n Nim :" << mhs1.nim;
+		cout << "\n nama :" << mhs1.nama;
+		cout << "\n alamat :" << mhs1.alamat;
+		cout << "\n umur :" << mhs1.umur;
+
+
+		cout << "\n Nim :" << mhs2.nim;
+		cout << "\n nama :" << mhs2.nama;
+		cout << "\n alamat :" << mhs2.alamat;
+		cout << "\n umur :" << mhs2.umur;
+		
+
+
+
 }
